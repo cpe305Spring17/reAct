5738bc2418786e7a72f5784bccf999fcc24471e7
Nishanth <nishanth.dara@gmail.com>
2017-04-27 17:48:03 -0700

    Added class stubs to model package

---
+++ app/src/main/java/edu/calpoly/react/model/Activity.java
@@ -0,0 +1,8 @@
+package edu.calpoly.react.model;
+
+/**
+ * Created by Nishanth on 4/27/17.
+ */
+
+public class Activity {
+}
---
+++ app/src/main/java/edu/calpoly/react/model/database/DBConnection.java
@@ -0,0 +1,8 @@
+package edu.calpoly.react.model.database;
+
+/**
+ * Created by Nishanth on 4/27/17.
+ */
+
+public class DBConnection {
+}
