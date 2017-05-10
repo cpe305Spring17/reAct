90e6d9fcfe78fabce6155ca82e72e8fb9170834b
Nishanth <nishanth.dara@gmail.com>
2017-05-10 14:08:55 -0700

    Added jacoco to .travis.yml and first unit test

---
+++ app/src/test/java/edu/calpoly/react/TestCategory.java
@@ -0,0 +1,16 @@
+package edu.calpoly.react;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.*;
+
+/**
+ * Created by Nishanth on 5/7/17.
+ */
+
+public class TestCategory {
+    @Test
+    public void TestActivityConstructor() {
+        assertTrue(true);
+    }
+}
